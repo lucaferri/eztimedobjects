b125389ab5d9a4bc0867a46accf2c3d9679efac6
Luca Ferri <luca.ferri88@gmail.com>
2012-07-06 13:59:43 +0200

    Add info

---
+++ ezinfo.php
@@ -0,0 +1,16 @@
+<?php
+
+class ggwebservicesInfo
+{
+    static function info()
+    {
+        return array(
+            'Name' => "<a href=\"https://github.com/lucaferri/eztimedobjects\">eztimedobjects extension</a>",
+            'Version' => "0.1-dev",
+            'Author' => "Luca Ferri (luca.ferri88@gmail.com)",
+            'License' => "GNU General Public License v2.0",
+        );
+    }
+}
+
+?>
