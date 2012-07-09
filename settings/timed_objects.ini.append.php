48acabe55e74d1ee810e65f374c404c410cc45fb
Luca Ferri <luca.ferri88@gmail.com>
2012-07-09 09:57:36 +0200

    Remove test data

--- settings/timed_objects.ini.append.php
+++ settings/timed_objects.ini.append.php
@@ -3,13 +3,11 @@
 [ExpireSettings]
 ClassAttribute[]
 #ClassAttribute[]=article/expires
-ClassAttribute[]=articolo/expires
 
 
 [PublishSettings]
 ClassAttribute[]
 #ClassAttribute[]=article/posted
-ClassAttribute[]=articolo/posted
 
 
 */?>
