a471e1edf407e3197a8c60082228c9124d3567aa
Luca Ferri <luca.ferri88@gmail.com>
2012-07-09 08:14:49 +0200

    0.2-dev Refactor timedobjectfunctioncollection.php, add expire_objects.sql

--- classes/timedobjectsfunctioncollection.php
+++ classes/timedobjectsfunctioncollection.php
@@ -2,27 +2,29 @@
 
 class timedObjectsFunctionCollection{
 	
+	// Get ExpireSettings from timed_objects.ini.append.php
 	public static function getExpireSettings(){
-		$ini = eZINI::instance('timed_objects.ini.append.php');
-		$ini = $ini->getNamedArray();
-		$expireSettings = $ini['ExpireSettings']['ClassAttribute'];
-		
-		$return = array();
-		foreach ($expireSettings as $setting){
-			$class_attribute = explode( '/', $setting );
-			$return = array_merge( $return, array(  $class_attribute[0] => $class_attribute[1] ) );
-		} 
-		
-		return $return;
+		return timedObjectsFunctionCollection::getSettings('Expire');
 	}
 	
+	// Get PublishSettings from timed_objects.ini.append.php
 	public static function getPublishSettings(){
+		return timedObjectsFunctionCollection::getSettings('Publish');
+	}
+	
+	// $type could be 'Expire' or 'Publish'
+	public static function getSettings( $type ){
+		
+		// Check if $type parameter is valid
+		if ( !in_array($type, array( 'Publish', 'Expire' )) )
+			return false;
+		
 		$ini = eZINI::instance('timed_objects.ini.append.php');
 		$ini = $ini->getNamedArray();
-		$publishSettings = $ini['PublishSettings']['ClassAttribute'];
+		$settings = $ini[$type."Settings"]['ClassAttribute'];
 	
 		$return = array();
-		foreach ($publishSettings as $setting){
+		foreach ($settings as $setting){
 			$class_attribute = explode( '/', $setting );
 			$return = array_merge( $return, array(  $class_attribute[0] => $class_attribute[1] ) );
 		}
--- sql/mysql/expire_objects.sql
+++ sql/mysql/expire_objects.sql
@@ -1,6 +1,6 @@
-CREATE TABLE IF NOT EXISTS `timed_objects` (
+CREATE TABLE IF NOT EXISTS `expire_objects` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `node_id` int(11) NOT NULL,
-  `time` int(11) NOT NULL,
+  `expire_on` int(11) NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
